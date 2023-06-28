1a64cc15841fdd57a00fe19e12e5ddd9ce02171b
Tibor Krajčovič <dev.tiqra@gmail.com>
2023-06-28 09:38:26 +0200

    basic shell

---
+++ src/components/Shell.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import TheHeader from './Header.vue'
+import TheFooter from './Footer.vue';
+</script>
+
+<template>
+    <div class="container mx-auto">
+        <header class="absolute top-1 h-8">
+            <TheHeader />
+        </header>
+        <main class="min-h-screen py-8">
+            <RouterView />
+        </main>
+        <footer class="absolute bottom-0 h-8">
+            <TheFooter />
+        </footer>
+    </div>
+</template>
+  
+<style>
+
+</style>
