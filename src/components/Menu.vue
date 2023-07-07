caf978206f52533761a484d54502086808255604
Tibor Krajčovič <dev.tiqra@gmail.com>
2023-07-07 17:37:11 +0200

    bocne menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import IconClose from '@/icons/IconClose.vue';
+import { computed } from 'vue';
+
+const props = defineProps(['opened'])
+const emit = defineEmits(['menu-close'])
+
+const close = () => {
+  emit('menu-close')
+}
+
+const closed = computed(()=> !props.opened)
+
+</script>
+
+<template>
+  <aside class="absolute top-0 bottom-0 left-0 right-0 bg-white transition-all transform duration-500" :class="{ closed }">
+    <button class="absolute top-0 right-0 pt-2 pr-2" @click="close">
+      <IconClose/>
+    </button>
+  </aside>
+</template>
+
+<style>
+.closed {
+  transform: translateX(-100%);
+}
+</style>
--- src/components/Shell.vue
+++ src/components/Shell.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
 import TheHeader from './Header.vue'
-import TheFooter from './Footer.vue';
+import Menu from './Menu.vue'
+import { ref } from 'vue'
+
+const menuOpened = ref(false)
+
+const openMenu = () => {
+    menuOpened.value = true;
+}
+
+const closeMenu = () => {
+    menuOpened.value = false;
+}
 </script>
 
 <template>
     <div class="container mx-auto">
         <header>
-            <TheHeader />
+            <TheHeader @menu-open="openMenu"/>
         </header>
         <main class="min-h-screen px-4">
             <RouterView />
         </main>
     </div>
+    <Menu :opened="menuOpened" @menu-close="closeMenu"/>
 </template>
   
 <style>
