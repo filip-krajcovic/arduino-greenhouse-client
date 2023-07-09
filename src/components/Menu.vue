376e9cd0652935d7c778b0827308bb53d45479f8
Filip Krajčovič <filipko.krajcovic@gmail.com>
2023-07-09 14:20:18 +0200

    Dizajn vysuvacieho menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import IconBook from '@/icons/IconBook.vue';
 import IconClose from '@/icons/IconClose.vue';
+import IconHistory2 from '@/icons/IconHistory2.vue';
+import IconNotification from '@/icons/IconNotification.vue';
+import IconProfile from '@/icons/IconProfile.vue';
+import IconRight from '@/icons/IconRight.vue';
+import IconSettings from '@/icons/IconSettings.vue';
 import { computed } from 'vue';
 
 const props = defineProps(['opened'])
@@ -15,9 +21,47 @@ const closed = computed(()=> !props.opened)
 
 <template>
   <aside class="absolute top-0 bottom-0 left-0 right-0 bg-white transition-all transform duration-500" :class="{ closed }">
-    <button class="absolute top-0 right-0 pt-2 pr-2" @click="close">
-      <IconClose/>
+    <button class="absolute top-0 right-0 pt-8 pr-6" @click="close">
+      <IconClose class="w-8 h-8"/>
     </button>
+    <h1 class="font-bold text-2xl pl-8 pb-5 pt-20">Ahoj, Filip!</h1>
+    <div class="flex items-center justify-between px-8 pb-8">
+      <div class="flex">
+        <IconProfile class="w-6 h-6"/>
+        <p class="pl-3 text-black/80 font-light">Môj účet</p>
+      </div>
+      <IconRight/>
+    </div>
+    <div class="h-px bg-gray-300"></div>
+    <div class="flex items-center justify-between px-8 pt-8">
+      <div class="flex">
+        <IconSettings class="w-6 h-6"/>
+        <p class="pl-3 text-base text-black/80 font-light">Nastavenia</p>
+      </div>
+      <IconRight/>
+    </div>
+    <div class="flex justify-between items-center pt-10 px-8">
+      <div class="flex">
+        <IconHistory2 class="w-6 h-6"/>
+        <p class="pl-3 text-base text-black/80 font-light">História</p>
+      </div>
+      <IconRight/>
+    </div>
+    <div class="flex items-center justify-between pt-10 px-8">
+      <div class="flex">
+        <IconNotification class="w-6 h-6"/>
+        <p class="pl-3 text-base text-black/80 font-light">Notifikácie</p>
+      </div>
+      <IconRight/>
+    </div>
+    <div class="flex items-center justify-between pt-10 px-8">
+      <div class="flex">
+        <IconBook class="w-6 h-6"/>
+        <p class="pl-3 text-base text-black/80 font-light">Katalóg</p>
+      </div>
+      <IconRight/>
+    </div>
+  
   </aside>
 </template>
 
