1cba58e8a291ac7075470afea37e3a8bd4d10eef
Filip Krajčovič <filipko.krajcovic@gmail.com>
2023-08-13 11:28:17 +0200

    Catalog

---
+++ src/components/Catalogue.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import IconLeft from '@/icons/IconLeft.vue';
+
+</script>
+
+
+<template>
+    <div class="pt-8 pb-10">
+        <span class="font-bold text-2xl text-gray-950/80">Katalóg</span>
+    </div>
+    
+</template>
