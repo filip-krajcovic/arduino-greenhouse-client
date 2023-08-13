fad4120c25bb33b1879e6b1ecb97bba053ac4606
Filip Krajčovič <filipko.krajcovic@gmail.com>
2023-08-13 15:23:34 +0200

    Hotovy katalog a pridanie notifikacii

--- src/components/Catalogue.vue
+++ src/components/Catalogue.vue
@@ -8,5 +8,67 @@ import IconLeft from '@/icons/IconLeft.vue';
     <div class="pt-8 pb-10">
         <span class="font-bold text-2xl text-gray-950/80">Katalóg</span>
     </div>
-    
+    <div class="grid grid-cols-2 grid-rows-3 gap-3">
+      <div
+        class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
+        <div class="flex flex-col items-center pt-12 pb-8">
+          <img src="src/assets/Rajcina.png" class="w-24 h-26" />
+        </div>
+        <div class="flex flex-col pl-6">
+          <h2 class="font-bold pb-1 text-black/90">Rajčiak jedlý</h2>
+          <span class="italic text-black/70 pb-4">Solanum lycopersicum</span>
+        </div>
+      </div>
+      <div
+        class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
+        <div class="flex flex-col items-center pt-2 pb-8">
+          <img src="src/assets/Paprika.png" class="w-22 h-28" />
+        </div>
+        <div class="flex flex-col pl-6">
+          <h2 class="font-bold pb-1 text-black/90">Paprika ročná</h2>
+          <span class="italic text-black/70 pb-4">Capsicum annuum</span>
+        </div>
+      </div>
+      <div
+        class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
+        <div class="flex flex-col items-center pt-8 pb-8">
+          <img src="src/assets/Chilli.png" class="h-32" />
+        </div>
+        <div class="flex flex-col pl-6">
+          <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
+          <span class="italic text-black/70 pb-4">Capsicum</span>
+        </div>
+      </div>
+      <div
+        class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
+        <div class="flex flex-col items-center pt-8 pb-8">
+          <img src="src/assets/Chilli.png" class="h-32" />
+        </div>
+        <div class="flex flex-col pl-6">
+          <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
+          <span class="italic text-black/70 pb-4">Capsicum</span>
+        </div>
+      </div>
+      <div
+        class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
+        <div class="flex flex-col items-center pt-8 pb-8">
+          <img src="src/assets/Chilli.png" class="h-32" />
+        </div>
+        <div class="flex flex-col pl-6">
+          <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
+          <span class="italic text-black/70 pb-4">Capsicum</span>
+        </div>
+      </div>
+      <div
+        class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
+        <div class="flex flex-col items-center pt-8 pb-8">
+          <img src="src/assets/Chilli.png" class="h-32" />
+        </div>
+        <div class="flex flex-col pl-6">
+          <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
+          <span class="italic text-black/70 pb-4">Capsicum</span>
+        </div>
+      </div>
+    </div>
+
 </template>
--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import HistoryView from '@/views/HistoryView.vue'
 import CatalogueView from '@/views/CatalogueView.vue'
+import NotificationsView from '@/views/NotificationsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/catalogue',
       name: 'catalogue',
       component: CatalogueView,
+    },
+    {
+      path: '/notifications',
+      name: 'notifications',
+      component: NotificationsView,
     },
   ]
 })
