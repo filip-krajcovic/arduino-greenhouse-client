a518e980863bf804b525e199f6a7dd4c9dadac56
Filip Krajčovič <filipko.krajcovic@gmail.com>
2023-08-14 10:43:39 +0200

    oprava buildu

--- src/components/Catalogue.vue
+++ src/components/Catalogue.vue
@@ -12,7 +12,7 @@ import IconLeft from '@/icons/IconLeft.vue';
       <div
         class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
         <div class="flex flex-col items-center pt-12 pb-8">
-          <img src="@/assets/Rajcina.png" class="w-24 h-26" />
+          <img src="../assets/RAJCINA.png" class="w-24 h-26" />
         </div>
         <div class="flex flex-col pl-6">
           <h2 class="font-bold pb-1 text-black/90">Rajčiak jedlý</h2>
@@ -22,7 +22,7 @@ import IconLeft from '@/icons/IconLeft.vue';
       <div
         class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
         <div class="flex flex-col items-center pt-2 pb-8">
-          <img src="@/assets/Paprika.png" class="w-22 h-28" />
+          <img src="../assets/PAPRIKA.png" class="w-22 h-28" />
         </div>
         <div class="flex flex-col pl-6">
           <h2 class="font-bold pb-1 text-black/90">Paprika ročná</h2>
@@ -32,7 +32,7 @@ import IconLeft from '@/icons/IconLeft.vue';
       <div
         class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
         <div class="flex flex-col items-center pt-8 pb-8">
-          <img src="@/assets/Chilli.png" class="h-32" />
+          <img src="../assets/CHILLI.png" class="h-32" />
         </div>
         <div class="flex flex-col pl-6">
           <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
@@ -42,7 +42,7 @@ import IconLeft from '@/icons/IconLeft.vue';
       <div
         class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
         <div class="flex flex-col items-center pt-8 pb-8">
-          <img src="@/assets/Chilli.png" class="h-32" />
+          <img src="../assets/CHILLI.png" class="h-32" />
         </div>
         <div class="flex flex-col pl-6">
           <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
@@ -52,7 +52,7 @@ import IconLeft from '@/icons/IconLeft.vue';
       <div
         class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
         <div class="flex flex-col items-center pt-8 pb-8">
-          <img src="@/assets/Chilli.png" class="h-32" />
+          <img src="../assets/CHILLI.png" class="h-32" />
         </div>
         <div class="flex flex-col pl-6">
           <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
@@ -62,7 +62,7 @@ import IconLeft from '@/icons/IconLeft.vue';
       <div
         class="flex justify-center flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-2xl">
         <div class="flex flex-col items-center pt-8 pb-8">
-          <img src="@/assets/Chilli.png" class="h-32" />
+          <img src="../assets/CHILLI.png" class="h-32" />
         </div>
         <div class="flex flex-col pl-6">
           <h2 class="font-bold pb-1 text-black/90">Chilli</h2>
