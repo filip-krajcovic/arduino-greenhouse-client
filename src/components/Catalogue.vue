5ea95db637f1c55cd54212ebaaf8b666366daf0b
Filip Krajčovič <filipko.krajcovic@gmail.com>
2023-08-16 18:10:11 +0200

    Pridanie obrazkov do katalogu

--- src/components/Catalogue.vue
+++ src/components/Catalogue.vue
@@ -2,7 +2,11 @@
 import IconLeft from '@/icons/IconLeft.vue'
 import rajcinaImageUrl from '../assets/RAJCINA.png'
 import paprikaImageUrl from '../assets/PAPRIKA.png'
-import chilliImageUrl from '../assets/CHILLI.png'
+import chilliImageUrl from '../assets/Chilli2.png'
+import bazalkaImageUrl from '../assets/Bazalka.png'
+import mataImageUrl from '../assets/Mata.png'
+import redkovkaImageUrl from '../assets/Redkovka.png'
+
 
 const items = [
   {
@@ -27,31 +31,31 @@ const items = [
       sk: 'Chilli',
       latin: 'Capsicum',
     },
-    class: 'h-24'
+    class: 'w-24 h-18'
   },
   {
-    url: rajcinaImageUrl,
+    url: bazalkaImageUrl,
     name: {
-      sk: 'Rajčiak jedlý',
-      latin: 'Solanum lycopersicum',
+      sk: 'Bazalka pravá',
+      latin: 'Ocimum basilicum',
     },
-    class: 'w-20 h-22 pt-4'
+    class: 'w-28 h-20'
   },
   {
-    url: paprikaImageUrl,
+    url: mataImageUrl,
     name: {
-      sk: 'Paprika ročná',
-      latin: 'Capsicum annuum',
+      sk: 'Mäta pieporná',
+      latin: 'Mentha piperita ',
     },
-    class: 'w-18 h-24'
+    class: 'w-28 h-20'
   },
   {
-    url: chilliImageUrl,
+    url: redkovkaImageUrl,
     name: {
-      sk: 'Chilli',
-      latin: 'Capsicum',
+      sk: 'Reďkev siata',
+      latin: 'Raphanus sativus',
     },
-    class: 'h-24'
+    class: 'h-28 w-22' 
   },
 ]
 
@@ -64,7 +68,7 @@ const items = [
         <span class="font-bold text-2xl text-gray-950/80">Katalóg</span>
     </div>
     <div class="grid grid-cols-2 grid-rows-3 gap-3 px-2">
-      <div v-for="item in items" class="grid  items-center px-4 py-4 flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-lg">
+      <div v-for="item in items" class="grid  items-center px-5 py-5 flex-col shadow-[0px_0px_15px_-3px_rgba(0,0,0,0.15)] rounded-lg">
         <div class="flex justify-center pb-6">
           <img :src="item.url" :class="item.class ?? 'w-24 h-26'" />
         </div>
