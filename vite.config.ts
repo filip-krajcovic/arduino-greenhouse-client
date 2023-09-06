87b9baf24ea41f22409081728c15c2c610aed0b6
Tibor Krajčovič <dev.tiqra@gmail.com>
2023-09-06 11:47:27 +0200

    added pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,56 @@ import vue from '@vitejs/plugin-vue'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import nodePolyfills from 'rollup-plugin-polyfill-node'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'Arduino',
+        short_name: 'Arduino',
+        description: 'Arduino greenhouse app',
+        theme_color: '#155e75',
+        icons: [
+          {
+            src: 'pwa-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
