d6461e9da33198dfcb84373bf9040fc0e0830223
Tibor Krajčovič <dev.tiqra@gmail.com>
2024-01-29 21:21:34 +0100

    add clientId to env vars

--- src/plugins/mqtt/mqtt.options.ts
+++ src/plugins/mqtt/mqtt.options.ts
@@ -7,6 +7,7 @@ const port = import.meta.env.VITE_MQTT_PORT
 const protocol = import.meta.env.VITE_MQTT_PROTOCOL
 const username = import.meta.env.VITE_MQTT_USERNAME
 const password = import.meta.env.VITE_MQTT_PASSWORD
+const clientId = import.meta.env.VITE_MQTT_CLIENT_ID
 
 export const clientOptions: IClientOptions = {
   path,
@@ -14,4 +15,5 @@ export const clientOptions: IClientOptions = {
   protocol,
   username,
   password,
+  clientId,
 }
